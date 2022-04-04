1719fd7238541da9b8781c54319f996cfcdea612
Aloknath-sah <kumaraditya6396@gmail.com>
2022-04-04 16:17:20 +0530

    implemented join css page and created link in login btn

--- my-chat-app/src/component/Join/Join.js
+++ my-chat-app/src/component/Join/Join.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Join.css';
 import logo from '../../images/logo.png';
+import {Link} from 'react-router-dom';
 
 const Join = () => {
   return (
@@ -9,6 +10,10 @@ const Join = () => {
         <div className='JoinContainer'>
           <img src={logo} alt="logo" />
           <h1>Join Page</h1>
+          <input type="text" id="joinInput" />
+          <Link to="/chat">
+            <button className='JoinBtn' >Log in</button>
+          </Link>
         </div>
       </div>
     </div>
