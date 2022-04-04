db058b148892d0a02622fd6c827e0b834b0955da
Aloknath-sah <kumaraditya6396@gmail.com>
2022-04-04 17:24:13 +0530

    applied css of chat component and connected server

--- my-chat-app/src/component/Join/Join.js
+++ my-chat-app/src/component/Join/Join.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Join.css';
 import logo from '../../images/logo.png';
 import {Link} from 'react-router-dom';
 
+let user;
+
+const sendUser = () => {
+  user = document.getElementById("joinInput").value;
+  document.getElementById("joinInput").value = "";
+}  
+
 const Join = () => {
+  
+  const [name, setname] = useState("");
+
   return (
     <div>
       <div className='JoinPage'>
         <div className='JoinContainer'>
           <img src={logo} alt="logo" />
           <h1>Join Page</h1>
-          <input type="text" id="joinInput" />
-          <Link to="/chat">
-            <button className='JoinBtn' >Log in</button>
+          <input type="text" onChange={(e) => setname(e.target.value)} id="joinInput" />
+          <Link onClick={(event) => !name? event.preventDefault():null} to="/chat">
+            <button onClick={sendUser} className='JoinBtn' >Log in</button>
           </Link>
         </div>
       </div>
@@ -21,3 +31,4 @@ const Join = () => {
 }
 
 export default Join
+export {user}
--- my-chat-app/src/component/chat/Chat.js
+++ my-chat-app/src/component/chat/Chat.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { user } from '../Join/Join';
+import socketIo from 'socket.io-client';
+import "./Chat.css";
+
+const ENDPOINT = "http://localhost:4500/";
 
 const Chat = () => {
+
+  const socket = socketIo(ENDPOINT, { transports: ['websocket']} );
+
+  useEffect(() => {
+    socket.on('connect', () => {
+        alert("connected");
+    })
+  
+    return () => {
+      
+    }
+  }, [socket])
+  
   return (
-    <div>chat</div>
+    <div className='chatPage'>
+        <div className='chatContainer'>
+            <div className='header'></div>
+            <div className='chatBox'></div>
+            <div className='inputBox'>
+                {user}
+            </div>
+        </div>
+    </div>
   )
 }
 
