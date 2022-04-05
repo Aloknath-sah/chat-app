a1862146f8d6955adcb795c2b362064d619ff7fc
Aloknath-sah <kumaraditya6396@gmail.com>
2022-04-05 09:03:00 +0530

    chat connected with server through socket.io

--- my-chat-app/src/component/chat/Chat.js
+++ my-chat-app/src/component/chat/Chat.js
@@ -1,26 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { user } from '../Join/Join';
 import socketIo from 'socket.io-client';
 import "./Chat.css";
 import sendLogo from '../../images/send.png';
 
 const ENDPOINT = "http://localhost:4500/";
+let socket;
 
 const Chat = () => {
+  const [id, setid] = useState("");
 
-  const socket = socketIo(ENDPOINT, { transports: ['websocket']} );
+  const send = () => {
+    const message = document.getElementById('chatInput').value;
+    socket.emit('message', {message, id});
+    document.getElementById('chatInput').value = "";
+  }
 
   useEffect(() => {
+    socket = socketIo(ENDPOINT, { transports: ['websocket']} );
     socket.on('connect', () => {
         alert("connected");
+        setid(socket.id);
     })
 
     socket.emit('joined', {user})
+
+    socket.on('welcome', (data) => {
+      console.log(data.user, data.message)
+    })
+
+    socket.on('userJoined', (data) => {
+      console.log(data.user, data.message)
+    })
+
+    socket.on('leave', (data) => {
+      console.log(data.user, data.message)
+    })
+  
+    return () => {
+      socket.emit('disconnect1');
+      socket.off();
+    }
+  }, [])
+
+  useEffect(() => {
+    socket.on('sendMessage', (data) => {
+      console.log(data.user, data.message)
+    });
   
     return () => {
       
     }
   }, [])
+  
   
   return (
     <div className='chatPage'>
@@ -29,7 +61,7 @@ const Chat = () => {
             <div className='chatBox'></div>
             <div className='inputBox'>
                 <input type="text" id="chatInput" />
-                <button className='sendBtn'>
+                <button onClick={send} className='sendBtn'>
                     <img src={sendLogo} alt="Send" />
                 </button>
             </div>
--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const socketIO = require("socket.io");
 const app = express();
 const port = 4500 || process.env.PORT
 
+const users1 = [{}];
+
 app.use(cors())
 app.get("/", (req, res) => {
     res.send("It's working");
@@ -19,8 +21,21 @@ io.on("connection", (socket) => {
     console.log("New Connection");
 
     socket.on("joined", (users) => {
+        users1[socket.id] = users.user;
         console.log(users.user)
+        socket.broadcast.emit('userJoined', {user:'Admin', message:`${users1[socket.id]} User has joined`})
+        socket.emit('welcome', {user:'admin', message:'welcome to the chat'})
     })
+
+    socket.on('message', ({message, id}) => {
+        io.emit('sendMessage', {user:users1[id], message})
+    })
+
+    socket.on('disconnect1', () => {
+        socket.broadcast.emit('leave', {user: 'admin', message:`${users1[socket.id]} has left`})
+        console.log('user left')
+    })
+
 })
 
 server.listen(port, () => {
